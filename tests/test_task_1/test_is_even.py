fa69aeadefd942fd97f6f1192a79680441e31445
StackCanary <danizmd8@gmail.com>
2024-10-03 12:49:59 +0300

    test: add tests from readme.md file (6, -6, 7, -7)

--- tests/test_task_1/test_is_even.py
+++ tests/test_task_1/test_is_even.py
@@ -3,14 +3,36 @@ from task_1.main import is_even_classic, is_even_bitwise
 
 
 @pytest.mark.parametrize(
-    "test_input, expected", [(1, False), (0, True), (2, True), (-1, False), (-2, True)]
+    "test_input, expected",
+    [
+        (1, False),
+        (0, True),
+        (2, True),
+        (-1, False),
+        (-2, True),
+        (6, True),
+        (-6, True),
+        (7, False),
+        (-7, False),
+    ],
 )
 def test_is_even_classic(test_input, expected):
     assert is_even_classic(test_input) == expected
 
 
 @pytest.mark.parametrize(
-    "test_input, expected", [(1, False), (0, True), (2, True), (-1, False), (-2, True)]
+    "test_input, expected",
+    [
+        (1, False),
+        (0, True),
+        (2, True),
+        (-1, False),
+        (-2, True),
+        (6, True),
+        (-6, True),
+        (7, False),
+        (-7, False),
+    ],
 )
 def test_is_even_bitwise(test_input, expected):
     assert is_even_bitwise(test_input) == expected
