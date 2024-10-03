7b36d861f5821cbe61506da9e6b09dfdc18b40fb
StackCanary <danizmd8@gmail.com>
2024-10-03 18:43:15 +0300

    test: move guppy related stuff into run_memory_profiler module

--- run_memory_profiler.py
+++ run_memory_profiler.py
@@ -1,4 +1,35 @@
-from tests.test_task_2.benchmarks import run_memory_profiler
+from task_2.array_circular_buffer import ArrayCircularBuffer
+from task_2.list_circular_buffer import ListCircularBuffer
+from guppy import hpy
+
+
+def memory_profile_list() -> None:
+    queue = ListCircularBuffer(10000)
+
+    for i in range(5000):
+        queue.push(i)
+
+    for i in range(2000):
+        queue.pop()
+
+    print(hpy().heap())
+
+
+def memory_profile_array() -> None:
+    queue = ArrayCircularBuffer(10000, "L")
+
+    for i in range(5000):
+        queue.push(i)
+
+    for i in range(2000):
+        queue.pop()
+
+    print(hpy().heap())
+
+
+def run_memory_profiler():
+    memory_profile_list()
+    memory_profile_array()
 
 
 if __name__ == "__main__":
--- tests/test_task_2/benchmarks.py
+++ tests/test_task_2/benchmarks.py
@@ -1,8 +1,4 @@
 from timeit import timeit
-from guppy import hpy
-
-from task_2.array_circular_buffer import ArrayCircularBuffer
-from task_2.list_circular_buffer import ListCircularBuffer
 
 
 def run_task_2_benchmarks(number_of_iterations: int) -> None:
@@ -20,32 +16,3 @@ def run_task_2_benchmarks(number_of_iterations: int) -> None:
             number=number_of_iterations,
         )
     )
-
-
-def memory_profile_list() -> None:
-    queue = ListCircularBuffer(10000)
-
-    for i in range(5000):
-        queue.push(i)
-
-    for i in range(2000):
-        queue.pop()
-
-    print(hpy().heap())
-
-
-def memory_profile_array() -> None:
-    queue = ArrayCircularBuffer(10000, "L")
-
-    for i in range(5000):
-        queue.push(i)
-
-    for i in range(2000):
-        queue.pop()
-
-    print(hpy().heap())
-
-
-def run_memory_profiler():
-    memory_profile_list()
-    memory_profile_array()
