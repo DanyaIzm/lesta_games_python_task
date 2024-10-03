ce580f87a950fc5cd8f95bdd76132e974642b143
StackCanary <danizmd8@gmail.com>
2024-10-03 18:38:57 +0300

    test: improve 1st task tests with number of iterations

--- tests/test_task_1/benchmarks.py
+++ tests/test_task_1/benchmarks.py
@@ -1,18 +1,18 @@
 from timeit import timeit
 
 
-def run_task_1_benchmarks():
+def run_task_1_benchmarks(number_of_iterations: int) -> None:
     print(
         timeit(
             "is_even_classic(10002)",
             setup="from task_1.main import is_even_classic",
-            number=1_000_000_000,
+            number=number_of_iterations,
         )
     )
     print(
         timeit(
             "is_even_bitwise(10002)",
             setup="from task_1.main import is_even_bitwise",
-            number=1_000_000_000,
+            number=number_of_iterations,
         )
     )
